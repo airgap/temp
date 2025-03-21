acebda13c8cc1d5fc6c215dd3c833afadd2f9659
Nicole Muzzin <nicole@muzz.in>
2025-03-20 21:30:58 -0400

    stuff

--- libs/si-bits/src/Search/Search.svelte
+++ libs/si-bits/src/Search/Search.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import BubbleButton from '../BubbleButton/BubbleButton.svelte';
-import styles from './Search.module.sass';
-import search from '../assets/search.svg?raw';
+	import styles from './Search.module.sass';
+	import search from '../assets/search.svg?raw';
 </script>
+
 <BubbleButton>{@html search}</BubbleButton>
 <!-- <span class={styles.Search}>
 	<input type="text" placeholder="Search..." />
