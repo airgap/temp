a4e857a67993b2af0a21486efd0a5e103924d588
Nicole Muzzin <nicole@muzz.in>
2025-05-11 20:03:11 -0400

    Fix vite.config.ts

--- libs/handles/vite.config.ts
+++ libs/handles/vite.config.ts
@@ -4,7 +4,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
 	root: __dirname,
-	cacheDir: '../../node_modules/.vite/libs/json-models',
+	cacheDir: '../../node_modules/.vite/libs/handles',
 	plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
 	// Uncomment this if you are using workers.
 	// worker: {
@@ -17,7 +17,7 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		reporters: ['default'],
 		coverage: {
-			reportsDirectory: '../../coverage/libs/json-models',
+			reportsDirectory: '../../coverage/libs/handles',
 			provider: 'v8',
 		},
 	},
---
+++ libs/ttf-ais/src/index.ts
@@ -0,0 +1,11 @@
+import { Redis, RedisOptions } from 'ioredis';
+
+const endpoint = process.env['REDIS_CONNECTION_STRING'];
+if (!endpoint) throw new Error('REDIS_CONNECTION_STRING not set!');
+export const createRedisClient = (props: RedisOptions = {}) =>
+	new Redis(endpoint, {
+		...props,
+		password: process.env['REDIS_PASSWORD'],
+		username: process.env['REDIS_USERNAME'],
+	});
+export const client = createRedisClient();
