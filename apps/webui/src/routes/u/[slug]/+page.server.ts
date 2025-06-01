690e0dc7e0fa516ab38facc2f6ee17a61074c457
airgap <nicole@muzz.in>
2025-05-31 22:26:02 -0400

    idk

--- apps/webui/src/routes/u/[slug]/+page.server.ts
+++ apps/webui/src/routes/u/[slug]/+page.server.ts
@@ -1,82 +1,19 @@
-// import type { PageLoad } from './$types';
-import { initRedis } from '../../../initRedis.server';
-import { neon, getUsers, getLikesForPosts } from '../../../lib/server';
-import { type User } from '@lyku/json-models';
-import { parsePossibleBON } from 'from-schema';
-import { Err } from '@lyku/helpers';
+import { decode, encode } from '@msgpack/msgpack';
 export const load = async ({ params, fetch, parent }) => {
-	const getUserBySlugFromPg = () =>
-		db
-			.selectFrom('users')
-			.selectAll()
-			.where('slug', '=', slug.toLocaleLowerCase())
-			.executeTakeFirst();
-	const { user, redis, pg } = await parent();
-	const db = pg;
-	const { slug } = params;
-	let target: User | null = null;
-	let userId = await redis.hget('userIds', slug);
-	if (!userId) {
-		const target = await getUserBySlugFromPg();
-		if (!target) throw new Err(404);
-		void redis.hset('users', target.id.toString(), JSON.stringify(target));
-		void redis.hset('userIds', slug, target.id.toString());
+	const body = encode({ post: params.slut }, { useBigInt64: true });
+	console.log('body', body);
+	const res = fetch('https://api.lyku.org/list-user-posts-with-meta', {
+		method: 'POST',
+		body,
+	})
+	if (!res.ok) {
+		throw new Error(`API request failed: ${res.status} ${res.statusText}`);
 	}
-	if (userId)
-		target = await redis.hget('users', userId).then(parsePossibleBON<User>);
-	if (!target) {
-		if (!target) throw new Err(404);
-		// if (!userId) await redis.hset('userIds', slug, target.id.toString());
-	}
-	const postIds = await redis.zrange(`user:${target}:recentPosts`, 0, 50, {
-		rev: true,
-		withScores: true,
-	});
-	const posts = target
-		? await db
-				.selectFrom('posts')
-				.selectAll()
-				.where('author', '=', target.id)
-				.orderBy('publish', 'desc')
-				.execute()
-		: [];
 
-	const authors = posts?.length
-		? await getUsers(
-				db,
-				redis,
-				posts.map((p) => p.id),
-			)
-		: [];
-	const rawFollows = await db
-		.selectFrom('userFollows')
-		.select('followee')
-		.where('follower', '=', user.id)
-		.where(
-			'followee',
-			'in',
-			authors.map((a) => a.id),
-		)
-		.execute();
-	const followVectors = authors.map((a) =>
-		rawFollows.some((f) => f.followee === a.id) ? a.id : -a.id,
-	);
-	const rawLikes = await getLikesForPosts(
-		db,
-		redis,
-		posts.map((p) => p.id),
-		user,
-	);
-	const likeVectors = posts.map((p) =>
-		rawLikes.some((l) => l === p.id) ? p.id : -p.id,
-	);
-	// const likemap = likes.reduce((o,l)=>({...o, [l.postId]}))
-	// console.log('OY M8 WE GOT LIKES', )
-	return {
-		posts,
-		users: authors,
-		likes: likeVectors,
-		follows: followVectors,
-		target,
-	};
+	const buffer = await res.arrayBuffer();
+	const response = decode(new Uint8Array(buffer), {
+		useBigInt64: true,
+	}) as any;
+
+	return response;
 };
